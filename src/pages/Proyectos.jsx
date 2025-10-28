41cf5050fa10c35b25ddeb3dd9159367fafd7916
David <davidferrersapinya@gmail.com>
2025-10-28 20:40:47 +0100

    Añadido proyecto FerMar

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,4 +1,5 @@
-import { motion } from  "framer-motion";
+import { motion } from "framer-motion";
+import Fermar from "../assets/Fermar.png";
 export default function Proyectos() {
   return (
     <motion.section
@@ -7,7 +8,6 @@ export default function Proyectos() {
       transition={{ duration: 0.8, ease: "easeOut" }}
       className="min-h-[calc(100vh-6rem)] bg-gradient-to-b from-sky-100 via-white to-sky-200 text-slate-800 px-6 py-16 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 dark:text-sky-100"
     >
-   
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-3xl sm:text-4xl font-bold text-sky-700 mb-4">
           Mis proyectos
@@ -26,7 +26,8 @@ export default function Proyectos() {
               </h3>
               <p className="text-slate-700 text-sm leading-relaxed">
                 Gestión de hielo y cajas. <br />
-                <span className="font-medium">Stack:</span> Spring Boot + MySQL + React
+                <span className="font-medium">Stack:</span> Spring Boot + MySQL
+                + React
               </p>
             </div>
             <a
@@ -51,7 +52,7 @@ export default function Proyectos() {
               </p>
             </div>
             <a
-              href="https://github.com/David24977/FallaApp"
+              href={"https://github.com/David24977/FallaApp"}
               target="_blank"
               rel="noopener noreferrer"
               className="mt-6 inline-block bg-sky-600 text-white text-sm font-medium px-4 py-2 rounded-lg hover:bg-sky-700 transition"
@@ -68,7 +69,8 @@ export default function Proyectos() {
               </h3>
               <p className="text-slate-700 text-sm leading-relaxed">
                 Backend con envío de notificaciones a Firebase. <br />
-                <span className="font-medium">Stack:</span> Spring Boot y Android Studio con Kotlin
+                <span className="font-medium">Stack:</span> Spring Boot y
+                Android Studio con Kotlin
               </p>
             </div>
             <a
@@ -79,6 +81,44 @@ export default function Proyectos() {
             >
               Ver en GitHub
             </a>
+          </div>
+
+          {/* Web FerMar */}
+          <div className="bg-white/70 backdrop-blur rounded-xl shadow-md hover:shadow-lg p-6 transition flex flex-col justify-between">
+            <div>
+              <img
+                src={Fermar}
+                alt="Logo Construcciones FerMar"
+                className="w-full h-40 object-contain rounded-md mb-4 bg-white/70 backdrop-blur"
+              />
+
+              <h3 className="text-xl font-semibold text-sky-700 mb-2">
+                Construcciones FerMar
+              </h3>
+              <p className="text-slate-700 text-sm leading-relaxed">
+                Sitio web profesional para una empresa de construcción local.
+                Incluye galería con carrusel, integración con Vercel y diseño
+                responsive moderno.
+              </p>
+            </div>
+            <div className="mt-6 flex gap-3 justify-center">
+              <a
+                href="https://construcciones-fermar.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-yellow-400 text-slate-900 text-sm font-medium px-4 py-2 rounded-lg hover:bg-yellow-300 transition"
+              >
+                Ver web
+              </a>
+              <a
+                href="https://github.com/David24977/construcciones-fermar"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-sky-600 text-white text-sm font-medium px-4 py-2 rounded-lg hover:bg-sky-700 transition"
+              >
+                Ver en Github
+              </a>
+            </div>
           </div>
         </div>
       </div>
